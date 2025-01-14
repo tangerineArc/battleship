eae119379488e5477ab170faa1b9cab7f8b3dda6
Swagatam <swagatamheroz@outlook.com>
2025-01-15 01:13:32 +0530

    feat: set up functionality to change orientation

--- src/index.js
+++ src/index.js
@@ -4,14 +4,18 @@ import "./style.css";
 
 import {
   gameScreen,
-  board1Cells as board1Cells_DOM
+  board1Cells as board1Cells_DOM,
 } from "./dom-cache/game-screen.js";
 import { startingScreen, startButton } from "./dom-cache/startingScreen.js";
 
 import GameBoard from "./models/game-board.js";
 import Ship from "./models/ship.js";
 
-import { BOARD_DIMENSION } from "./globals/constants.js";
+import {
+  BOARD_DIMENSION,
+  HORIZONTAL_ORIENTATION,
+  VERTICAL_ORIENTATION,
+} from "./globals/constants.js";
 
 startButton.addEventListener("click", () => {
   startingScreen.style.display = "none";
@@ -56,10 +60,80 @@ for (let i = 0; i < player1Ships.length; i++) {
   );
 }
 
+renderBoard1();
+
 for (let i = 0; i < BOARD_DIMENSION; i++) {
   for (let j = 0; j < BOARD_DIMENSION; j++) {
-    if (player1Board.board[i][j].ship) {
-      board1Cells_DOM[i][j].classList.add("ship-cell");
+    board1Cells_DOM[i][j].addEventListener("click", () => {
+      if (!player1Board.board[i][j].ship) return;
+
+      let startPos = [i, j];
+      let idx = player1Board.board[i][j].index;
+      while (idx--) {
+        if (player1Board.board[i][j].orientation === HORIZONTAL_ORIENTATION) {
+          startPos[1]--;
+        } else {
+          startPos[0]--;
+        }
+      }
+
+      let viablePositions = player1Board.getPlaceablePositions(player1Board.board[i][j].ship);
+
+      if (player1Board.board[i][j].orientation === HORIZONTAL_ORIENTATION) {
+        for (
+          let a = startPos[0] + 1;
+          a < startPos[0] + player1Board.board[i][j].ship.dimension;
+          a++
+        ) {
+          if (!viablePositions.includes(JSON.stringify([a, startPos[1]]))) {
+            return;
+          }
+        }
+
+        const ship = player1Board.board[i][j].ship;
+        player1Board.removeShip(ship);
+        player1Board.placeShip(
+          ship,
+          startPos,
+          [startPos[0] + ship.dimension - 1, startPos[1]],
+        );
+        renderBoard1();
+
+        player1Board.board[i][j].orientation = VERTICAL_ORIENTATION;
+      } else {
+        for (
+          let a = startPos[1] + 1;
+          a < startPos[1] + player1Board.board[i][j].ship.dimension;
+          a++
+        ) {
+          if (!viablePositions.includes(JSON.stringify([startPos[0], a]))) {
+            return;
+          }
+        }
+
+        const ship = player1Board.board[i][j].ship;
+        player1Board.removeShip(ship);
+        player1Board.placeShip(
+          ship,
+          startPos,
+          [startPos[0], startPos[1] + ship.dimension - 1],
+        );
+        renderBoard1();
+
+        player1Board.board[i][j].orientation = HORIZONTAL_ORIENTATION;
+      }
+    });
+  }
+}
+
+function renderBoard1() {
+  for (let i = 0; i < BOARD_DIMENSION; i++) {
+    for (let j = 0; j < BOARD_DIMENSION; j++) {
+      if (player1Board.board[i][j].ship) {
+        board1Cells_DOM[i][j].classList.add("ship-cell");
+      } else {
+        board1Cells_DOM[i][j].classList.remove("ship-cell");
+      }
     }
   }
 }
--- src/models/game-board.js
+++ src/models/game-board.js
@@ -1,6 +1,10 @@
 "use strict";
 
-import { BOARD_DIMENSION } from "../globals/constants.js";
+import {
+  BOARD_DIMENSION,
+  HORIZONTAL_ORIENTATION,
+  VERTICAL_ORIENTATION,
+} from "../globals/constants.js";
 
 export default class GameBoard {
   constructor() {
@@ -8,7 +12,12 @@ export default class GameBoard {
     for (let i = 0; i < BOARD_DIMENSION; i++) {
       const row = [];
       for (let j = 0; j < BOARD_DIMENSION; j++) {
-        row.push({ ship: null, isHit: false });
+        row.push({
+          ship: null,
+          isHit: false,
+          index: null,
+          orientation: null,
+        });
       }
       board.push(row);
     }
@@ -21,6 +30,8 @@ export default class GameBoard {
       throw new Error("Invalid start and end positions");
     }
 
+    let index = 0;
+
     if (startPos[0] === endPos[0]) {
       if (ship.dimension !== endPos[1] - startPos[1] + 1) {
         throw new Error("Incompatible ship and positions");
@@ -28,6 +39,8 @@ export default class GameBoard {
 
       for (let i = startPos[1]; i <= endPos[1]; i++) {
         this.board[startPos[0]][i].ship = ship;
+        this.board[startPos[0]][i].index = index++;
+        this.board[startPos[0]][i].orientation = HORIZONTAL_ORIENTATION;
       }
     } else if (startPos[1] === endPos[1]) {
       if (ship.dimension !== endPos[0] - startPos[0] + 1) {
@@ -36,8 +49,61 @@ export default class GameBoard {
 
       for (let i = startPos[0]; i <= endPos[0]; i++) {
         this.board[i][startPos[1]].ship = ship;
+        this.board[i][startPos[1]].index = index++;
+        this.board[i][startPos[1]].orientation = VERTICAL_ORIENTATION;
       }
     }
+  }
+
+  removeShip(ship) {
+    for (let i = 0; i < BOARD_DIMENSION; i++) {
+      for (let j = 0; j < BOARD_DIMENSION; j++) {
+        if (this.board[i][j].ship === ship) {
+          this.board[i][j].ship = null;
+          this.board[i][j].index = null;
+          this.board[i][j].orientation = null;
+        }
+      }
+    }
+  }
+
+  getPlaceablePositions(ship) {
+    const positions = [];
+    for (let i = 0; i < BOARD_DIMENSION; i++) {
+      for (let j = 0; j < BOARD_DIMENSION; j++) {
+        const cell = this.board[i][j];
+        if (
+          !(
+            (cell.ship && cell.ship !== ship) ||
+            (this.board[i][j + 1]?.ship &&
+              this.board[i][j + 1]?.ship !== ship) ||
+            (this.board[i][j - 1]?.ship &&
+              this.board[i][j - 1]?.ship !== ship) ||
+            (this.board[i + 1] &&
+              this.board[i + 1][j].ship &&
+              this.board[i + 1][j].ship !== ship) ||
+            (this.board[i - 1] &&
+              this.board[i - 1][j].ship &&
+              this.board[i - 1][j].ship !== ship) ||
+            (this.board[i + 1] &&
+              this.board[i + 1][j + 1]?.ship &&
+              this.board[i + 1][j + 1]?.ship !== ship) ||
+            (this.board[i + 1] &&
+              this.board[i + 1][j - 1]?.ship &&
+              this.board[i + 1][j - 1]?.ship !== ship) ||
+            (this.board[i - 1] &&
+              this.board[i - 1][j + 1]?.ship &&
+              this.board[i - 1][j + 1]?.ship !== ship) ||
+            (this.board[i - 1] &&
+              this.board[i - 1][j - 1]?.ship &&
+              this.board[i - 1][j - 1]?.ship !== ship)
+          )
+        ) {
+          positions.push(JSON.stringify([i, j]));
+        }
+      }
+    }
+    return positions;
   }
 
   receiveAttack([i, j]) {
